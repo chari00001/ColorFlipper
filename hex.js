945e4d9630c11be32ff798e90f66c8bc429f33be
chari00001 <90192334+chari00001@users.noreply.github.com>
2022-05-29 05:48:22 +0300

    ColorFlipper
    random color flipper with js

---
+++ hex.js
@@ -0,0 +1,21 @@
+const hexColors = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    let hex = "#";
+    let randomHex;
+
+    for (let i = 0; i < 6; i++) {
+        randomHex = hexColors[getRandomNumber()];
+        hex += randomHex;
+    }
+
+    color.textContent = hex;
+    document.body.style.backgroundColor = hex;
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*hexColors.length);
+}
